661b52e02a3baca15a4e34ab862bbbc1f3a049f7
AEDXDEV <103389504+AEDXDEV@users.noreply.github.com>
2024-07-21 10:32:41 +0300

    Update Main.php

--- src/AEDXDEV/Combat/Main.php
+++ src/AEDXDEV/Combat/Main.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace AEDXDEV\Combat;
@@ -6,140 +7,172 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\entity\ProjectileHitEntityEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\entity\projectile\Arrow;
 use pocketmine\player\Player;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
 
 class Main extends PluginBase implements Listener {
+  
+  use SingletonTrait;
+  
   // [Player1:Player2 => Time]
-  public array $combat = [];
+  private array $combat = [];
   
-  public Config $config;
-  public bool $Enable = true;
-  public int $Time = 10;
+  private Config $config;
   
-  public static $instance;
-  
-  public function onLoad(): void{
-		self::$instance = $this;
-	}
-	
-	public static function getInstance(): Main{
-		return self::$instance;
-	}
+  private bool $isEnable = true;
+  private bool $sameCombat = false;
+  private bool $sendMessages = true;
+  private array $messages = [];
+  private int $combatTime = 10;
   
 	public function onEnable(): void{
+	  self::setInstance($this);
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$config = new Config($this->getDataFolder() . "config.yml", 2, [
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
 		  "Enable" => true,
-		  "Time" => 10
+		  "CancelIfNotInSameCombat" => false,
+		  "sendMessages" => true,
+		  "Messages" => [
+		    "Start" => "§eYou are now in combat with {PLAYER}",
+		    "InSameCombat" => "§cYou cannot proceed because you are not fighting the same opponent. Please focus on your current combat!",
+		    "End" => "§eYou are no longer in combat."
+		  ],
+		  "Time" => 10,
 		]);
 		$this->config = $config;
-	  $this->Enable = $config->get("Enable", false);
-	  $this->Time = $config->get("Time", 10);
-	  $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(static function(): void{
-	    Main::$instance->CombatTask();
+	  $this->isEnable = $config->get("Enable", false);
+	  $this->sameCombat = $config->get("CancelIfNotInSameCombat", false);
+	  $this->sendMessages = $config->get("sendMessages", false);
+	  $this->messages = $config->get("Messages", [
+	    "Start" => "§eYou are now in combat with {PLAYER}",
+	    "InSameCombat" => "§cYou cannot proceed because you are not fighting the same opponent. Please focus on your current combat!",
+	    "End" => "§eYou are no longer in combat."
+	  ]);
+	  $this->combatTime = $config->get("Time", 10);
+	  $this->getScheduler()->scheduleRepeatingTask(new ClosureTask(function(): void{
+	    $this->handleCombatTask();
 		}), 20);
 	}
 	
-	public function onDamage(EntityDamageEvent $event){
-		$entity = $event->getEntity();
-	  if($event instanceof EntityDamageByEntityEvent && ($damager = $event->getDamager()) instanceof Player && ($entity = $event->getEntity()) instanceof Player && $this->Enable){
-	    $this->addCombat($damager, $entity);
-		}
+	public function onDamage(EntityDamageEvent $event): void{
+	  if ($event instanceof EntityDamageByEntityEvent) {
+	    $damager = $event->getDamager();
+	    $entity = $event->getEntity();
+	    if (!$this->isEnabled || $event->isCancelled())return;
+	    if ($damager instanceof Player && $entity instanceof Player) {
+	      if ($player->isCreative() || $damager->isCreative())return;
+	      if($entity->getHealth() <= $event->getFinalDamage()){
+	        $this->removeCombat($entity);
+	        return;
+	      }
+	      if ($this->isInSameCombat($damager, $entity) && $this->sameCombat) {
+	        if ($this->sendMessages) {
+	          $player->sendMessage($this->messages["InSameCombat"]);
+	        }
+	        $event->cancel();
+	        return;
+	      }
+	      $this->addCombat($damager, $entity);
+	    }
+	  }
 	}
 	
-	public function onProjectileHit(ProjectileHitEntityEvent $event) {
-    if(($arrow = $event->getEntity()) instanceof Arrow && ($owner = $arrow->getOwningEntity()) instanceof Player && ($target = $event->getEntityHit()) instanceof Player && $this->Enable) {
-      $this->addCombat($owner, $target);
+	public function onProjectileHit(ProjectileHitEntityEvent $event): void{
+	  $projectile = $event->getEntity();
+	  if ($projectile instanceof Arrow) {
+	    $owner = $projectile->getOwningEntity();
+	    $target = $event->getEntityHit();
+	    if (!$this->isEnabled || $event->isCancelled())return;
+	    if ($owner instanceof Player && $target instanceof Player) {
+	      if ($this->isInSameCombat($owner, $target) && $this->sameCombat) {
+	        if ($this->sendMessages) {
+	          $player->sendMessage($this->messages["InSameCombat"]);
+	        }
+	        $event->cancel();
+	        return;
+	      }
+	      $this->addCombat($owner, $target);
+	    }
+	  }
+  }
+  
+  public function onQuit(PlayerQuitEvent $event){
+    $player = $event->getPlayer();
+    if ($this->isInCombat($player)) {
+      $this->removeCombat($player);
+    }
+  }
+  
+  private function isInCombat(Player $player): bool{
+	  $name = $player->getName();
+	  foreach ($this->combat as $key => $time) {
+	    if (strpos($key, $name) !== false) {
+	      return true;
+	    }
+	  }
+    return false;
+  }
+	
+	public function addCombat(Player $damager, Player $target): void{
+	  $key = $damager->getName() . ":" . $target->getName();
+	  if ($this->isInCombat($damager) || $this->isInCombat($target)) {
+	    return;
+	  }
+	  $this->combat[$key] = $this->combatTime;
+	  if ($this->sendMessages) {
+	    $msg = str_replace("{PLAYER}", "", $this->messages["Start"]);
+	    $damager->sendMessage($msg . $targer->getName());
+	    $target->sendMessage($msg . $damager->getName());
+	  }
+  }
+  
+  public function getPlayerCombat(Player $player): ?Player{
+    $playerName = $player->getName();
+    $target = null;
+    foreach ($this->combat as $key => $time) {
+      [$name1, $name2] = explode(":", $key);
+      if ($playerName === $name1) {
+        $target = $name1;
+      }
+      if ($playerName === $name2) {
+        $target = $name2;
+      }
+    }
+    return $target !== null ? $this->getServer()->getPlayerExact($target) : null;
+  }
+  
+  public function isInSameCombat(Player $player1, Player $player2): bool{
+    $key1 = $player1->getName() . ":" . $player2->getName();
+    $key2 = $player2->getName() . ":" . $player1->getName();
+    return isset($this->combat[$key1]) || isset($this->combat[$key2]);
+  }
+  
+  public function removeCombat(Player $player): void{
+    $name = $player->getName();
+    foreach ($this->combat as $key => $time) {
+      if (strpos($key, $name) !== false) {
+        unset($this->combat[$key]);
+        if ($this->sendMessages) {
+    	    $player->sendMessage($this->messages["End"]);
+    	    $p = $this->getServer()->getPlayerExact(str_replace([$name, ":"], "", $key));
+    	    $p->sendMessage($this->messages["End"]);
+    	  }
+      }
     }
   }
 	
-	public function onDeath(PlayerDeathEvent $event) {
-	  $player = $event->getPlayer();
-	  if ($this->hasCombat($player)) {
-	    $this->unCombat($player);
-	  }
-	}
-  
-	public function addCombat(Player $damager, Player $entity) {
-	  $players = $damager->getName() . ":" . $entity->getName();
-	  if ($this->hasCombat($damager) || $this->hasCombat($entity)){
-	    $this->unCombatName($players);
-	  }
-	  $this->combat[$players] = $this->Time;
-	}
-	
-	public function addCombatName(string $players) {
-	  $p = explode(":", $players);
-	  if ($this->hasCombatName($p[0]) || $this->hasCombatName($p[1])){
-	    $this->unCombatName($players);
-	  }
-	  $this->combat[$players] = $this->Time;
-	}
-	
-	public function getPlayerCombat(Player $player): ?Player{
-	  $player_ = null;
-	  $name = $player->getName();
-		foreach($this->combat as $players => $time) {
-		  $p = explode(":", $players);
-		  if ($name === $p[0]) {
-		    $player_ = $this->getServer()->getPlayerExact($p[1]);
-		  }
-		  if ($name === $p[1]) {
-		    $player_ = $this->getServer()->getPlayerExact($p[0]);
-		  }
-		}
-		return $player_;
-	}
-	
-	public function unCombat(Player $player) {
-	  if (!$this->hasCombat($player))return false;
-	  $name = $player->getName();
-		foreach($this->combat as $players => $time) {
-		  $p = explode(":", $players);
-		  if ($name === $p[0] or $name === $p[1]) {
-		    unset($this->combat[$players]);
-		  }
-		}
-	}
-	
-	public function unCombatName(string $players) {
-	  $p = explode(":", $players);
-	  if (!$this->hasCombatName($p[0]) && !$this->hasCombatName($p[1]))return false;
-		unset($this->combat[$players]);
-	}
-	
-	public function hasCombat(Player $player): bool{
-	  $name = $player->getName();
-	  foreach ($this->combat as $players => $time) {
-	    $p = explode(":", $players);
-	    if ($name === $p[0] or $name === $p[1]) {
-	      return true;
+	private function handleCombatTask(): void{
+	  foreach ($this->combat as $key => $time) {
+	    if ($time <= 0) {
+	      $this->removeCombatByKey($key);
+	    } else {
+	      $this->combat[$key]--;
 	    }
 	  }
-	  return false;
-	}
-	
-	public function hasCombatName(string $name): bool{
-	  foreach ($this->combat as $players => $time) {
-	    $p = explode(":", $players);
-	    if ($name === $p[0] or $name === $p[1]) {
-	      return true;
-	    }
-	  }
-	  return false;
-	}
-	public function CombatTask() {
-	  foreach ($this->combat as $players => $time){
-      if($time == 0){
-        $this->unCombatName($players);
-        } else {
-          --$this->combat[$players];
-        }
-		}
 	}
 }
