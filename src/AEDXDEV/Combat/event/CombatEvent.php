e49cfaef125d7a4fadab95ad1a692b73f683ffa2
AEDXDEV <103389504+AEDXDEV@users.noreply.github.com>
2024-08-19 00:22:58 +0300

    Fix

--- src/AEDXDEV/Combat/event/CombatEvent.php
+++ src/AEDXDEV/Combat/event/CombatEvent.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace AEDXDEV\Combat\event;
-
 declare(strict_types=1);
+
+namespace AEDXDEV\Combat\event;
 
 use pocketmine\event\Event;
 use pocketmine\player\Player;
--- src/AEDXDEV/Combat/event/CombatStartEvent.php
+++ src/AEDXDEV/Combat/event/CombatStartEvent.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace AEDXDEV\Combat\event;
-
 declare(strict_types=1);
+
+namespace AEDXDEV\Combat\event;
 
 use pocketmine\event\Cancellable;
 use pocketmine\event\CancellableTrait;
