3028761e89bf70b290d1a5d804e896188c774f25
AEDXDEV <103389504+AEDXDEV@users.noreply.github.com>
2024-08-18 21:01:01 +0300

    Add Events

---
+++ src/AEDXDEV/Combat/event/CombatEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AEDXDEV\Combat\event;
+
+declare(strict_types=1);
+
+use pocketmine\event\Event;
+use pocketmine\player\Player;
+
+class CombatEvent extends Event{
+  // Nothing is Here
+}
